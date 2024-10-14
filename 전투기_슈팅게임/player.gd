77c454259c0911719f492a065643a392a62bcf98
kimke11 <lgu27670644@gmail.com>
2024-10-14 13:52:22 +0900

    2024_10_14

---
+++ 전투기_슈팅게임/player.gd
@@ -0,0 +1,45 @@
+extends RigidBody2D
+
+enum {INIT, ALIVE, INVULNERABLE, DEAD}
+
+var state = INIT
+
+@export var engine_power = 500
+@export var spin_power = 8000
+
+var thrust = Vector2.ZERO
+var rotation_dir = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	chang_state(ALIVE)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	get_input()
+
+func chang_state(new_state):
+	match new_state:
+		INIT:
+			$CollisionShape2D.set_deferred("disabled",true)
+		ALIVE:
+			$CollisionShape2D.set_deferred("disabled",false)
+		INVULNERABLE:
+			$CollisionShape2D.set_deferred("disabled",true)
+		DEAD:
+			$CollisionShape2D.set_deferred("disabled",true)
+	state = new_state
+
+func get_input():
+	thrust = Vector2.ZERO
+	if state in [DEAD, INIT]:
+		return
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	
+func _physics_process(delta: float) -> void:
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
